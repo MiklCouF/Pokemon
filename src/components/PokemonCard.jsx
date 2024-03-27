accc5caaad1af6f19016f41a15cf09fb0f25393d
Michaël Couffignal <michael@couffignal.fr>
2024-03-27 16:05:32 +0100

    add PokemonCard

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,15 @@
+function PokemonCard() {
+
+    return (
+        <figure>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+            <figcaption>
+                Bulbasaur
+            </figcaption>
+        </figure>
+    );
+
+}
+
+
+export default PokemonCard;
