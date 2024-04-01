5bb3812b4304b5cfc052152535e8b8a7589aa107
Michaël Couffignal <michael@couffignal.fr>
2024-04-01 20:51:53 +0200

    first props

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,23 @@ import PokemonCard from './components/PokemonCard.jsx'
 
 function App() {
 
+  const pokemonList = [
+
+    {
+      name: "bulbasaur",
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+
+    {
+      name: "mew",
+    },
+  ];
+
   return (
 
     <div>
 
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[0]} />
 
     </div>
 
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,26 +1,12 @@
-function PokemonCard() {
-
-    const pokemonList = [
-
-        {
-            name: "bulbasaur",
-            imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-        },
-
-        {
-            name: "mew",
-        },
-    ];
-
-
-    let pokemon = pokemonList[1];
+function PokemonCard({pokemon}) {
 
     return (
         <>
 
-            {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt="" /> : <p>???</p>}
-            <p>{pokemon.name}</p>
+         {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt="" /> : <p>???</p>}
 
+<p>{pokemon.name}</p>
+            <figcaption>ed</figcaption>
         </>
     );
 
